8bd23d65ba058a26ace3c8a14e6ce55f3e662d99
Pedro Barbiero <pedrohenriquebarbiero@hotmail.com>
2025-08-04 18:50:21 -0600

    Refactor RequestResult to simplify constructor and improve IsValid property logic

--- Src/Framework/Framework.Application/Requests/RequestResult.cs
+++ Src/Framework/Framework.Application/Requests/RequestResult.cs
@@ -4,26 +4,25 @@ namespace Framework.Application.Requests;
 
 public class RequestResult<T>
 {
-    private RequestResult(T? data, IDictionary<string, string[]> errors, bool isValid, ushort statusCode)
+    private RequestResult(T? data, IDictionary<string, string[]> errors, ushort statusCode)
     {
         Data = data;
         Errors = errors;
-        IsValid = isValid;
         StatusCode = statusCode;
     }
 
     public T? Data { get; }
-    public bool IsValid { get; }
+    public bool IsValid => Errors.Count == 0;
     public ushort StatusCode { get; }
     public IDictionary<string, string[]> Errors { get; }
-    public static RequestResult<T> Ok(T data) => new(data, new Dictionary<string, string[]>(), true, 200);
-    public static RequestResult<T> Created(T data) => new(data, new Dictionary<string, string[]>(), true, 201);
-    public static RequestResult<T> NoContent() => new(default(T), new Dictionary<string, string[]>(), true, 204);
+    public static RequestResult<T> Ok(T data) => new(data, new Dictionary<string, string[]>(), 200);
+    public static RequestResult<T> Created(T data) => new(data, new Dictionary<string, string[]>(), 201);
+    public static RequestResult<T> NoContent() => new(default, new Dictionary<string, string[]>(), 204);
 
     public static RequestResult<T> BadRequest(ValidationResult validationResult) =>
-        new(default, validationResult.Errors, false, 400);
+        new(default, validationResult.Errors, 400);
 
     public static RequestResult<T> NotFound(string className, Guid id) =>
         new(default, new Dictionary<string, string[]> { { "NotFound", [$"{className} not found.", id.ToString()] } },
-            false, 404);
+            404);
 }
