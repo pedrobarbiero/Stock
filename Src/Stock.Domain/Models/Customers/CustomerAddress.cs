8bd0dba605e95a7fca2782ee7b70b0e52f981d60
Pedro Barbiero <pedrohenriquebarbiero@hotmail.com>
2025-08-04 20:56:23 -0600

    Refactor CustomerAddress properties to use init-only setters

--- Src/Stock.Domain/Models/Customers/CustomerAddress.cs
+++ Src/Stock.Domain/Models/Customers/CustomerAddress.cs
@@ -5,33 +5,7 @@ namespace Stock.Domain.Models.Customers;
 public class CustomerAddress : Entity
 {
     public Guid CustomerId { get; init; }
-
-    public string? Street
-    {
-        get => _street;
-        init => _street = value;
-    }
-
-    public string? City
-    {
-        get => _city;
-        init => _city = value;
-    }
-
-    public string? PostalCode
-    {
-        get => _postalCode;
-        init => _postalCode = value;
-    }
-
-    private string? _street;
-    private string? _city;
-    private string? _postalCode;
-
-    public void Update(string? street, string? city, string? postalCode)
-    {
-        _street = street;
-        _city = city;
-        _postalCode = postalCode;
-    }
+    public string? Street { get; init; }
+    public string? City { get; init; }
+    public string? PostalCode { get; init; }
 }
