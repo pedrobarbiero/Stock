af18d5a29e56179589c1fefb8408bfa2c04891a5
Pedro Barbiero <pedrohenriquebarbiero@hotmail.com>
2025-08-13 21:36:50 -0600

    Add Hangfire background job scheduler and service collection extension

---
+++ Src/Stock.Infrastructure.BackgroundJobs.Hangfire/HangfireBackgroundJobScheduler.cs
@@ -0,0 +1,30 @@
+using System.Linq.Expressions;
+
+using Framework.Application.BackgroundJobs;
+
+using Hangfire;
+
+namespace Stock.Infrastructure.BackgroundJobs.Hangfire;
+
+public class HangfireBackgroundJobScheduler(IBackgroundJobClient backgroundJobClient) : IBackgroundJobScheduler
+{
+    private readonly IBackgroundJobClient _backgroundJobClient =
+        backgroundJobClient ?? throw new ArgumentNullException(nameof(backgroundJobClient));
+
+    public Guid Enqueue<T>(Expression<Func<T, Task>> methodCall)
+    {
+        var jobId = _backgroundJobClient.Enqueue(methodCall);
+        return Guid.Parse(jobId);
+    }
+
+    public Guid Schedule<T>(Expression<Func<T, Task>> methodCall, TimeSpan delay)
+    {
+        var jobId = _backgroundJobClient.Schedule(methodCall, delay);
+        return Guid.Parse(jobId);
+    }
+
+    public bool Delete(Guid jobId)
+    {
+        return _backgroundJobClient.Delete(jobId.ToString());
+    }
+}
---
+++ Src/Stock.Infrastructure.BackgroundJobs.Hangfire/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+using Framework.Application.BackgroundJobs;
+
+using Hangfire;
+using Hangfire.PostgreSql;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Stock.Infrastructure.BackgroundJobs.Hangfire;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddHangfireBackgroundJobs(this IServiceCollection services,
+        string connectionString)
+    {
+        services.AddHangfire(configuration => configuration
+            .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+            .UseSimpleAssemblyNameTypeSerializer()
+            .UseRecommendedSerializerSettings()
+            .UsePostgreSqlStorage(c => c.UseNpgsqlConnection(connectionString)));
+
+        services.AddHangfireServer();
+
+        services.AddScoped<IBackgroundJobScheduler, HangfireBackgroundJobScheduler>();
+
+        return services;
+    }
+}
